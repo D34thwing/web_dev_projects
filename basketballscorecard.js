60260713d7870c234d6dcf39802f5b16f3df50d8
D34thwing <jatinjoshi20096@acropolis.in>
2023-02-25 23:37:54 +0530

    initial commit

---
+++ basketballscorecard.js
@@ -0,0 +1,26 @@
+let countH=0
+function addH1(){
+    countH+=1;
+    document.getElementById("homeScore").innerText=countH;
+}
+function addH2(){
+    countH+=2;
+    document.getElementById("homeScore").innerText=countH;
+}
+function addH3(){
+    countH+=3;
+    document.getElementById("homeScore").innerText=countH;
+}
+let countG=0
+function addG1(){
+    countG+=1;
+    document.getElementById("guestScore").innerText=countG;
+}
+function addG2(){
+     countG+=2;
+    document.getElementById("guestScore").innerText=countG;
+}
+function addG3(){
+     countG+=3;
+    document.getElementById("guestScore").innerText=countG;
+}
